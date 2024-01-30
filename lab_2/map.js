8e6b7015d78826af3e37e829b19ec2965462a679
Madeline Meltesen <157516209+meltesen5706@users.noreply.github.com>
2024-01-30 11:18:40 -0600

    Add files via upload

---
+++ lab_2/map.js
@@ -0,0 +1,15 @@
+var mymap = L.map("map").setView([41.981484, -88.29953], 13);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZ2NoYXVkaHVyaSIsImEiOiJjazBtcG5odG8wMDltM2JtcjdnYTgyanBnIn0.qwqjMomdrBMG36GQKXBlMw', {
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1
+}).addTo(mymap);
+
+L.marker([41.981484, -88.29953])
+    .addTo(mymap)
+    .bindPopup("<b>Hello!</b><br>This is my house where I grew up.")
+    .openPopup();
