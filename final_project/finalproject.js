b395d9967f0fc1d7502468f4823adbc54f946ca7
Madeline Meltesen <157516209+meltesen5706@users.noreply.github.com>
2024-02-29 13:01:40 -0600

    Add files via upload

---
+++ final_project/finalproject.js
@@ -0,0 +1,11 @@
+var mymap = L.map("map").setView([38.852501192, -96.608159610], 5);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZ2NoYXVkaHVyaSIsImEiOiJjazBtcG5odG8wMDltM2JtcjdnYTgyanBnIn0.qwqjMomdrBMG36GQKXBlMw', {
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1
+}).addTo(mymap);
+
