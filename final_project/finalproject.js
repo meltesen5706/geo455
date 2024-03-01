f207aa468f808421cd538ecba42d3ad32118487b
Madeline Meltesen <157516209+meltesen5706@users.noreply.github.com>
2024-02-29 19:20:56 -0600

    Add files via upload

--- final_project/finalproject.js
+++ final_project/finalproject.js
@@ -1,11 +1,15 @@
-var mymap = L.map("map").setView([38.852501192, -96.608159610], 5);
+// Wait for the DOM to be fully loaded before initializing the map
+document.addEventListener("DOMContentLoaded", function() {
+    // Create a map centered at the specified coordinates
+    var mymap = L.map("map").setView([38.852501192, -96.608159610], 5);
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZ2NoYXVkaHVyaSIsImEiOiJjazBtcG5odG8wMDltM2JtcjdnYTgyanBnIn0.qwqjMomdrBMG36GQKXBlMw', {
-    maxZoom: 18,
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
-        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1
-}).addTo(mymap);
-
+    // Add a tile layer to the map
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZ2NoYXVkaHVyaSIsImEiOiJjazBtcG5odG8wMDltM2JtcjdnYTgyanBnIn0.qwqjMomdrBMG36GQKXBlMw', {
+        maxZoom: 18,
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+            'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1
+    }).addTo(mymap);
+});
