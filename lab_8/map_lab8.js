a80d43fc3a66c9b716fe4fc52d644642c3f32d2f
Madeline Meltesen <157516209+meltesen5706@users.noreply.github.com>
2024-03-28 10:34:43 -0500

    Add files via upload

--- lab_8/map_lab8.js
+++ lab_8/map_lab8.js
@@ -1,13 +1,11 @@
 var mymap = L.map("map").setView([38.852501192, -96.608159610], 5);
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZ2NoYXVkaHVyaSIsImEiOiJjazBtcG5odG8wMDltM2JtcjdnYTgyanBnIn0.qwqjMomdrBMG36GQKXBlMw', {
-    maxZoom: 18,
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
-        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1
-}).addTo(mymap);
+
+ var grayscale = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
+   attribution: 'Map tiles by Carto, under CC BY 3.0. Data by OpenStreetMap, under ODbL.',
+   maxZoom: 11,
+   minZoom: 2
+ }).addTo(mymap);
 
 
 // jQuery code for buttons
